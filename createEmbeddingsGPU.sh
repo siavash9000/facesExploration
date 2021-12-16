a105361a8d8cc0a55dbb109485fad5d39b637a5c
Siavash Sefid Rodi <sefidrodi@googlemail.com>
2021-12-16 09:22:51 +0100

    added notebooks + minor changes

--- createEmbeddingsGPU.sh
+++ createEmbeddingsGPU.sh
@@ -2,4 +2,4 @@
 cd faceEmbedder
 docker build -t gpu_embedding -f Dockerfile.gpu .
 cd ..
-docker run --gpus all -v ${PWD}/putHereYourInputImages:/inputdata -v ${PWD}/getHereYourResults:/output gpu_embedding
+docker run --rm -v ${PWD}/putHereYourInputImages:/inputdata -v ${PWD}/getHereYourResults:/output --gpus all  nvidia/cuda:9.0-cudnn7-runtime-ubuntu16.04 gpu_embedding:latest
